2a9cfcc7d4ecf33aaede0d60d213c8b1a62c2760
william <williamnjati420@gmail.com>
2025-12-04 19:58:10 +0300

    testing js

---
+++ script.js
@@ -0,0 +1,13 @@
+const oneButton = document.querySelector ("one")
+const onebuttonValue = oneButton.value();
+
+oneButton.addEventListener ('click', () => {
+    console.log (oneButton.value)
+})
+
+const twoButton = document.querySelector ("two")
+const twobuttonValue = twoButton.value();
+
+twoButton.addEventListener ('click', () => {
+    console.log ("william")
+})
