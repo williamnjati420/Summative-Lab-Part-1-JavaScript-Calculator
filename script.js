f5996076f206f06346d6f6523606756e0ff59df3
william <williamnjati420@gmail.com>
2025-12-05 10:01:34 +0300

    added ids to buttons

--- script.js
+++ script.js
@@ -1,13 +1,151 @@
-const oneButton = document.querySelector ("one")
-const onebuttonValue = oneButton.value();
+// One button
 
-oneButton.addEventListener ('click', () => {
-    console.log (oneButton.value)
+const oneButton = document.getElementById ("one")
+
+oneButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+
+
 })
 
-const twoButton = document.querySelector ("two")
-const twobuttonValue = twoButton.value();
+// Two button
 
-twoButton.addEventListener ('click', () => {
-    console.log ("william")
+const twoButton = document.getElementById ("two")
+
+twoButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
 })
+
+// Three button
+
+const threeButton = document.getElementById ("three")
+
+threeButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Four button
+
+const fourButton = document.getElementById ("four")
+
+fourButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Five button
+
+const fiveButton = document.getElementById ("five")
+
+fiveButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Six button
+
+const sixButton = document.getElementById ("six")
+
+sixButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Seven button
+
+const sevenButton = document.getElementById ("seven")
+
+sevenButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Eight button
+
+const eightButton = document.getElementById ("eight")
+
+eightButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Nine button
+
+const nineButton = document.getElementById ("nine")
+
+nineButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Zero button
+
+const zeroButton = document.getElementById ("zero")
+
+zeroButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Addition button
+
+const additionButton = document.getElementById ("addition")
+
+additionButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Subtraction button
+
+const subtractionButton = document.getElementById ("subtraction")
+
+subtractionButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Multiplication button
+
+const multiplicationButton = document.getElementById ("multiplication")
+
+multiplicationButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Division button
+
+const divisionButton = document.getElementById ("division")
+
+divisionButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Point button
+
+const pointButton = document.getElementById ("point")
+
+pointButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Equals button
+
+const equalsButton = document.getElementById ("equals")
+
+equalsButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// allClear button
+
+const allClearButton = document.getElementById ("all-clear")
+
+allClearButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+// Delete button
+
+const deleteButton = document.getElementById ("delete")
+
+deleteButton.addEventListener ('click', e => {
+    console.log (e.target.innerHTML)
+})
+
+
+
+
+
+
